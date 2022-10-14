475fabedae03d43c54807d105cec56b611696d5d
RUPAM_PC\rupam <rdebmail@gmail.com>
2022-10-14 20:38:07 +0530

    No use of update(0 itself

--- Basketball3D/Assets/Scripts/ClickAndDrag.cs
+++ Basketball3D/Assets/Scripts/ClickAndDrag.cs
@@ -10,29 +10,29 @@ public class ClickAndDrag : MonoBehaviour
     {
         selectedObject.transform.position = new Vector3(0.02f, 0.44f, -7.49f);
     }
-    void Update()
-    {
-        //Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        //    Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
-        //    //Debug.Log(offset + " 1 23");
-        //    if (targetObject)
-        //    {
-        //        selectedObject = targetObject.transform.gameObject;
-        //        offset = selectedObject.transform.position - mousePosition;
-        //        //Debug.Log("Inside");
-        //    }
-        //}
-        //if (selectedObject)
-        //{
-        //    selectedObject.transform.position = mousePosition + offset;
-        //    Debug.Log(offset + " 1 23");
-        //}
-        //if (Input.GetMouseButtonUp(0) && selectedObject)
-        //{
-        //    Debug.Log("Outside");
-        //    selectedObject = null;
-        //}
-    }
+    //void Update()
+    //{
+    //    Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
+    //        //Debug.Log(offset + " 1 23");
+    //        if (targetObject)
+    //        {
+    //            selectedObject = targetObject.transform.gameObject;
+    //            offset = selectedObject.transform.position - mousePosition;
+    //            //Debug.Log("Inside");
+    //        }
+    //    }
+    //    if (selectedObject)
+    //    {
+    //        selectedObject.transform.position = mousePosition + offset;
+    //        Debug.Log(offset + " 1 23");
+    //    }
+    //    if (Input.GetMouseButtonUp(0) && selectedObject)
+    //    {
+    //        Debug.Log("Outside");
+    //        selectedObject = null;
+    //    }
+    //}
 }
